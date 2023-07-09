13f88b88fc46c6a8782c9d759228a0766fa63497
Vladimir Tishchenko <695amr@mail.ru>
2023-07-09 13:15:37 +0300

    создание проекта

---
+++ src/app/agreements-page/agreements-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agreements-page',
+  templateUrl: './agreements-page.component.html',
+  styleUrls: ['./agreements-page.component.scss']
+})
+export class AgreementsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
