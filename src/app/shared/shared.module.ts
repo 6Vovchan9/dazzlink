da091b2d13e2bcccaa834f8ba1dd36be56e977e7
Vladimir Tishchenko <695amr@mail.ru>
2023-08-18 23:55:50 +0300

    create loader and progress-spinner component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,22 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+
 import { SearchPipe } from './pipes/search.pipe';
+import { ProgressSpinnerComponent } from "./components/progress-spinner/progress-spinner.component";
+import { LoaderComponent } from "./components/loader/loader.component";
 
 @NgModule({
+    declarations: [
+      SearchPipe,
+      ProgressSpinnerComponent, // надо регистрировать тут потому что этот компонент будет исп-ся в app.module и admin.module
+      LoaderComponent
+    ],
     imports: [HttpClientModule],
     exports: [
       HttpClientModule,
-      SearchPipe
-    ],
-    declarations: [
-      SearchPipe
+      SearchPipe,
+      ProgressSpinnerComponent,
+      LoaderComponent
     ]
 })
 export class SharedModule { }
