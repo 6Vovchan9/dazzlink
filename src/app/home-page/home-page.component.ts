6bec425854596d64b129fe55954254d384e9b60e
Vladimir Tishchenko <695amr@mail.ru>
2023-10-10 00:15:22 +0300

    исправил ошибку progressive image и добавил главную картинку для мобилы

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -71,15 +71,10 @@ export class HomePageComponent implements OnInit, OnDestroy {
     // private cd: ChangeDetectorRef
   ) { }
 
-  @HostListener('window:load', [])
-  onWindowLoad() {
-    // console.log('onWindowLoad');
-    // const pageWrapEl = document.getElementById('pageWrap');
-    // console.dir(pageWrapEl);
-    // pageWrapEl.addEventListener('scroll', () => {
-    //   console.log('pageWrapScroll')
-    // })
-  }
+  // @HostListener('window:load', [])
+  // onWindowLoad() {
+  //   console.log('onWindowLoad');
+  // }
 
   public ngOnInit(): void {
     // console.log('---HomePageComponent init---');
@@ -95,88 +90,92 @@ export class HomePageComponent implements OnInit, OnDestroy {
 
   private aboutProgressiveImage(): void {
     if (window.addEventListener && window.requestAnimationFrame && document.getElementsByClassName) {
-
-      window.addEventListener('load', function () {
-
-        let pItem = document.getElementsByClassName('progressive replace'), timer;
-        const pageWrapEl = document.getElementById('pageWrap');
-
-        // window.addEventListener('scroll', scroller, false);
-        pageWrapEl.addEventListener('scroll', scroller, false);
-        window.addEventListener('resize', scroller, false);
-
-        // setTimeout(() => {
-        inView();
-        // }, 500);
-
-        function scroller(e) {
-          timer = timer || setTimeout(function() {
-            timer = null;
-            requestAnimationFrame(inView);
-          }, 300);
-        }
-
-        function inView() {
-
-          let wT = pageWrapEl.scrollTop, wB = wT + window.innerHeight, cRect, pT, pB, p = 0;
-          while (p < pItem.length) {
-      
-            cRect = pItem[p].getBoundingClientRect();
-            pT = wT + cRect.top; // расстояние с верха окна до картинки
-            pB = pT + cRect.height; // pT + высота картинки
-      
-            // console.log(wT, pB, wB, pT);
-            if (wT < pB && wB > pT) {
-              // console.log('Загружаем осн картинку');
-              loadFullImage(pItem[p]);
-              pItem[p].classList.remove('replace');
-            }
-            else p++;
-      
-          }
-        }
-
-        // replace with full image
-        function loadFullImage(item) {
-
-          if (!item || !item.href) return;
-
-          // load image
-          let img = new Image();
-          if (item.dataset) {
-            img.srcset = item.dataset.srcset || '';
-            img.sizes = item.dataset.sizes || '';
-          }
-          img.src = item.href;
-          img.className = 'reveal';
-          if (img.complete) addImg();
-          else img.onload = addImg;
-
-          // replace image
-          function addImg() {
-
-            // disable click
-            item.addEventListener('click', function (e) { e.preventDefault(); }, false);
-
-            // add full image
-            item.appendChild(img).addEventListener('animationend', function (e) {
-
-              // remove preview image
-              let pImg = item.querySelector && item.querySelector('img.preview');
-              if (pImg) {
-                e.target.alt = pImg.alt || '';
-                item.removeChild(pImg);
-                e.target.classList.remove('reveal');
-              }
-
-            });
-
-          }
-
-        }
-
-      }, false)
+      if (document.readyState === 'complete') {
+        this.onWindowLoaded();
+      } else {
+        window.addEventListener('load', this.onWindowLoaded, false);
+      }
     }
+  }
+
+  private onWindowLoaded(): void {
+    let pItem = document.getElementsByClassName('progressive replace'), timer;
+    const pageWrapEl = document.getElementById('pageWrap');
+
+    // window.addEventListener('scroll', scroller, false);
+    pageWrapEl.addEventListener('scroll', scroller, false);
+    window.addEventListener('resize', scroller, false);
+
+    // setTimeout(() => {
+    inView();
+    // }, 500);
+
+    function scroller(e) {
+      timer = timer || setTimeout(function() {
+        timer = null;
+        requestAnimationFrame(inView);
+      }, 300);
+    }
+
+    function inView() {
+
+      let wT = pageWrapEl.scrollTop, wB = wT + window.innerHeight, cRect, pT, pB, p = 0;
+      while (p < pItem.length) {
+  
+        cRect = pItem[p].getBoundingClientRect();
+        pT = wT + cRect.top; // расстояние с верха окна до картинки
+        pB = pT + cRect.height; // pT + высота картинки
+  
+        // console.log(wT, pB, wB, pT);
+        if (wT < pB && wB > pT) {
+          // console.log('Загружаем осн картинку');
+          loadFullImage(pItem[p]);
+          pItem[p].classList.remove('replace');
+        }
+        else p++;
+  
+      }
+    }
+
+    // replace with full image
+    function loadFullImage(item) {
+
+      if (!item || !item.href) return;
+
+      // load image
+      let img = new Image();
+      if (item.dataset) {
+        img.srcset = item.dataset.srcset || '';
+        img.sizes = item.dataset.sizes || '';
+      }
+      img.src = item.href;
+      img.className = 'reveal';
+      if (img.complete) addImg();
+      else img.onload = addImg;
+
+      // replace image
+      function addImg() {
+
+        // disable click
+        item.addEventListener('click', function (e) { e.preventDefault(); }, false);
+
+        // add full image
+        item.appendChild(img).addEventListener('animationend', function (e) {
+
+          // remove preview image
+          let pImg = item.querySelector && item.querySelector('img.preview');
+          if (pImg) {
+            e.target.alt = pImg.alt || '';
+            item.removeChild(pImg);
+            e.target.classList.remove('reveal');
+          }
+
+        });
+
+      }
+
+    }
+
   }
 
   public onAccoTriggerClick(path: Array<string>): void {
