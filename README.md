e6c8d4f80b79ae796fde042bcbc88414edc764ac
Tishchenko Vladimir <695amr@mail.ru>
2024-01-06 20:31:48 +0300

    корректное обращение к window в mobile-detect.service и подключил скрипт для управления telegram ботом

--- README.md
+++ README.md
@@ -39,8 +39,13 @@ ng new dazzlink -> удаляем ранее созданный бандл, ес
 
 ## firebase hosting
 
-Глобально должен быть установлен пакет npm install -g firebase-tools, далее firebase login -> firebase init -> удаляем ранее созданный бандл, если есть (/dist или /docs) -> собираем проект ng build --output-path docs -> firebase deploy (эта инструкция есть на https://firebase.google.com)
+Глобально должен быть установлен пакет npm install -g firebase-tools, далее firebase login -> firebase init -> удаляем ранее созданный бандл, если есть (/dist или /docs) -> собираем проект ng build --output-path docs -> firebase deploy (эта инструкция есть на https://firebase.google.com и у Владилен Минин на ютубе видео от 5.12.23)
 
 ## svg-spinners 
 
 множество спиннеров (загрузчиков) тут https://github.com/n3r4zzurr0/svg-spinners
+
+## telegram bot
+
+For a description of the Bot API, see this page: https://core.telegram.org/bots/api и небольшая инструкция у Владилен Минин на ютубе видео от 5.12.23.
+Базовый тг бот: t.me/TgDazzlinkMyBot
--- src/app/shared/services/mobile-detect.service.ts
+++ src/app/shared/services/mobile-detect.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+import { Inject, Injectable } from "@angular/core";
 
 @Injectable({
     providedIn: 'root'
@@ -7,15 +8,19 @@ export class MobileDetectService {
 
     public mobileOrTabletDevice = false;
     public osDevice: string;
+    private window: Window;
 
-    constructor() {
+    constructor(
+        @Inject(DOCUMENT) private _document
+    ) {
+        this.window = this._document.defaultView;
         this.operateUserAgent();
     }
 
     private operateUserAgent(): void {
-        const mdClass = window['MobileDetect']; // Этот класс берется из скрипта в index.html
+        const mdClass = this.window['MobileDetect']; // Этот класс берется из скрипта в index.html
         if (mdClass) {
-            const mbInstance = new mdClass(window.navigator.userAgent);
+            const mbInstance = new mdClass(this.window.navigator.userAgent);
             console.log("Mobile: " + mbInstance.mobile() + "; Phone: " + mbInstance.phone() + "; Tablet: " + mbInstance.tablet() + "; OS: " + mbInstance.os() + "; userAgent: " + mbInstance.userAgent());
             
             this.mobileOrTabletDevice = Boolean(mbInstance.mobile());
@@ -27,14 +32,14 @@ export class MobileDetectService {
         // if (this.osDevice?.toLowerCase()) { // если это планшет или телефон
             if (this.osDevice?.toLowerCase() === 'ios') {
                 // window.location.href = 'https://www.apple.com/app-store';
-                window.location.href = 'https://apps.apple.com/ru';
+                this.window.location.href = 'https://apps.apple.com/ru';
             } else if (this.osDevice?.toLowerCase() === 'androidos') {
                 // window.open('https://play.google.com', '_blank');
                 // window.location.href = 'https://play.google.com';
-                window.open('https://play.google.com');
+                this.window.open('https://play.google.com');
             } else {
                 // window.location.href = 'https://appgallery.huawei.com';
-                window.open('https://appgallery.huawei.com');
+                this.window.open('https://appgallery.huawei.com');
             }
         // } else { // если это комп или ноут
         //     window.open('https://www.apple.com/app-store');
