e02d598b553cfa4629560085086720e75ff1cac5
Tishchenko Vladimir <695amr@mail.ru>
2024-01-27 17:57:27 +0300

    работа над страницей "Помощь"

--- src/app/help-page/help-page.component.ts
+++ src/app/help-page/help-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-help-page',
   templateUrl: './help-page.component.html',
   styleUrls: ['./help-page.component.scss']
 })
-export class HelpPageComponent {
+export class HelpPageComponent implements OnInit {
 
+  private route: ActivatedRoute = inject(ActivatedRoute)
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.route.fragment.subscribe(data => {
+      if (data) this.jumpToSection(data);
+    })
+  }
+
+  private jumpToSection(section): void {
+    document.getElementById(section).scrollIntoView({behavior: 'smooth'})
+  }
 }
