d02a0544db7951e360f040f93514fd7319f7264e
Tishchenko Vladimir <695amr@mail.ru>
2024-03-13 23:13:02 +0300

    добавил градиент под скелетеоны локаций когда они грузятся

--- src/app/shared/mock/locations.ts
+++ src/app/shared/mock/locations.ts
@@ -4,6 +4,20 @@ export const MOCK_LOCATIONS_FOR_SKELETON: RovraggeRespLocationsData = {
     placeCount: 2,
     cityPlaceList: [
         {
+            "cityCode": "ALMATY",
+            "cityTitle": "Алматы",
+            "placeList": [
+                {
+                    "categoryCode": "RESTAURANTS",
+                    "title": "Старый город",
+                    "subtitle": "Европейская",
+                    "subcategory": "Ресторан",
+                    "rating": 4.2,
+                    "address": "проспект Ленина, 17",
+                }
+            ]
+        },
+        {
             cityCode: "TASHKENT",
             cityTitle: "Ташкент",
             placeList: [
@@ -25,20 +39,36 @@ export const MOCK_LOCATIONS_FOR_SKELETON: RovraggeRespLocationsData = {
                 }
             ]
         },
-        // {
-        //   "cityCode": "ALMATY",
-        //   "cityTitle": "Алматы",
-        //   "placeList": [
-        //     {
-        //       "categoryCode": "RESTAURANTS",
-        //       "title": "Старый город",
-        //       "subtitle": "Европейская",
-        //       "subcategory": "Ресторан",
-        //       "rating": 4.2,
-        //       "address": "проспект Ленина, 17",
-        //     }
-        //   ]
-        // }
+        {
+            "cityCode": "ASTANA",
+            "cityTitle": "Астана",
+            "placeList": [
+                {
+                    "categoryCode": "RESTAURANTS",
+                    "title": "La Riviere",
+                    "subtitle": "Итальянская",
+                    "subcategory": "Ресторан",
+                    "rating": 5,
+                    "address": "просп. Кабанбай Батыра, 1",
+                },
+                {
+                    "categoryCode": "RESTAURANTS",
+                    "title": "Ozen",
+                    "subtitle": "Европейская",
+                    "subcategory": "Ресто-бар",
+                    "rating": 3.9,
+                    "address": "ул. Достык, 16",
+                },
+                {
+                    "categoryCode": "RESTAURANTS",
+                    "title": "Zamin",
+                    "subtitle": "Европейская",
+                    "subcategory": "Кафе",
+                    "rating": 4.9,
+                    "address": "5-й микрорайон, 16",
+                }
+            ]
+        }
     ]
 }
 
