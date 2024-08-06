aa78d22b5c8598b6f758ca417c271653d9af3fde
Tishchenko Vladimir <695amr@mail.ru>
2024-08-06 12:02:27 +0300

    переключил все на тестовое api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,6 @@ export const environment: Environment = {
   fbDbUrl: 'https://dazzlink-e643b-default-rtdb.firebaseio.com',
   rovraggeUrl: 'https://rovragge.ru/dating/api/article-content',
   rovraggePlacesUrl: 'https://rovragge.ru/dating/api/meeting-places',
-  articleUrl: 'https://api.dazzlink.asia/article-content',
-  placeUrl: 'https://api.dazzlink.asia/meeting-places'
+  articleUrl: 'https://testapi.dazzlink.asia/article-content',
+  placeUrl: 'https://testapi.dazzlink.asia/meeting-places'
 };
