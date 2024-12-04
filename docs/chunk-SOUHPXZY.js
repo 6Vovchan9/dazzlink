af74612d266fb1229851b3cbdacfade3a913a658
Tishchenko Vladimir <695amr@mail.ru>
2024-12-05 00:12:48 +0300

    новый docs

---
+++ docs/chunk-SOUHPXZY.js
@@ -0,0 +1,1 @@
+var a={production:!0,apiKey:"AIzaSyCgmx_4eLW1JXVz-8qB8KMdbavDpYtKGSA",fbDbUrl:"https://dazzlink-e643b-default-rtdb.firebaseio.com",rovraggeUrl:"https://rovragge.ru/dating/api/article-content",rovraggePlacesUrl:"https://rovragge.ru/dating/api/meeting-places",articleUrl:"https://api.dazzlink.asia/article-content",placeUrl:"https://api.dazzlink.asia/meeting-places"};export{a};
