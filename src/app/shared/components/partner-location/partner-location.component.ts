da3df4fc402b0bdfb14473ba1fd1102d641d6e62
Tishchenko Vladimir <695amr@mail.ru>
2025-08-27 23:09:32 +0300

    feat: доделал фото партнеров на мобиле и разукрасил кнопку "Смотреть оффер"

--- src/app/shared/components/partner-location/partner-location.component.ts
+++ src/app/shared/components/partner-location/partner-location.component.ts
@@ -1,5 +1,5 @@
 import { SlicePipe, NgClass } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input, InputSignal, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, InputSignal } from '@angular/core';
 import { RespCityPlaceList } from '@app/shared/interfaces';
 import { ThumbHashImageComponent } from '../thumb-hash-image/thumb-hash-image.component';
 
@@ -13,7 +13,10 @@ import { ThumbHashImageComponent } from '../thumb-hash-image/thumb-hash-image.co
 })
 export class PartnerLocationComponent {
   partner: InputSignal<RespCityPlaceList> = input(null, { alias: 'data' });
-
+  partnerBtnText = 'Смотреть оффер';
+  openPartner(): void {
+    console.log('openPartner');
+  }
   operatePriceRange(num = 1): Array<any> {
     return new Array(+num > 3 ? 3 : +num || 3);
   }
