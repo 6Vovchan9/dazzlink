d61ec0d205b7bb3330a1c18cf69adc97cb93e689
Vladimir Tishchenko <v.tischenko@winline.ru>
2025-09-24 11:11:30 +0300

    chore: winline debugger

--- src/app/legal-info-page/legal-info-page.component.ts
+++ src/app/legal-info-page/legal-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { FooterComponent } from '@app/shared/components/footer/footer.component';
 import { HeaderComponent } from '@app/shared/components/header/header.component';
 
@@ -12,4 +12,66 @@ import { HeaderComponent } from '@app/shared/components/header/header.component'
     FooterComponent
   ]
 })
-export class LegalInfoPageComponent { }
+export class LegalInfoPageComponent implements OnInit {
+  showWinlineBtns = signal(false);
+  isTg = signal(false);
+
+  checkIsTg(): void {
+    const tgPlatform = !!(window.document.defaultView as any).Telegram?.WebApp;
+    this.isTg.set(tgPlatform);
+  }
+
+  ngOnInit() {
+    this.checkIsTg();
+  }
+
+  toWinlineOpenBlank(): void {
+    this.checkIsTg();
+    window.open('https://m.winline.ru/', '_blank');
+  }
+
+  toWinlineOpenSelf(): void {
+    this.checkIsTg();
+    window.open('https://m.winline.ru/', '_self');
+  }
+
+  toWinlineOpenParent(): void {
+    this.checkIsTg();
+    window.open('https://m.winline.ru/', '_parent');
+  }
+
+  toWinlineOpenTop(): void {
+    this.checkIsTg();
+    window.open('https://m.winline.ru/', '_top');
+  }
+
+  toWinlineHref(): void {
+    this.checkIsTg();
+    window.location.href = 'https://m.winline.ru/';
+  }
+
+  toTunaOpenBlank(): void {
+    this.checkIsTg();
+    window.open('https://tuna', '_blank');
+  }
+
+  toTunaOpenSelf(): void {
+    this.checkIsTg();
+    window.open('https://tuna', '_self');
+  }
+
+  toTunaOpenParent(): void {
+    this.checkIsTg();
+    window.open('https://tuna', '_parent');
+  }
+
+  toTunaOpenTop(): void {
+    this.checkIsTg();
+    window.open('https://tuna', '_top');
+  }
+
+  toTunaHref(): void {
+    this.checkIsTg();
+    window.location.href = 'https://tuna';
+  }
+}
